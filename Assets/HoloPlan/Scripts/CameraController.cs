8435f49975e00c1b46f429149f5ceb3d2ee48c99
INFORMATIK\6haselma <6haselma@informatik.uni-hamburg.de>
2018-10-27 17:46:19 +0200

    Testprojekt

---
+++ Assets/HoloPlan/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float rotationSpeed;
+
+    private Vector3 offset;
+    private float smooth = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        offset = transform.position - player.transform.position;
+	}
+
+    private void FixedUpdate()
+    {
+
+        while (Input.GetKey("q"))
+        {
+            Quaternion targetRot = Quaternion.Euler(0, rotationSpeed, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * smooth);
+        }
+        while (Input.GetKey("e"))
+        {
+            Quaternion targetRot = Quaternion.Euler(0, -rotationSpeed, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * smooth);
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+	}
+}
